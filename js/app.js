c952b964fdab199b4c3271155b2522f44237673f
Muhammad Askar <muhammadaskar74@gmail.com>
2021-02-21 04:20:11 +0800

    update index

---
+++ js/app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    data: {
+        projects: [
+            { name: 'Keluarga Besar Mahasiswa Pendidikan Teknelogi Informasi', class: "kbmpti", img: "img/projects/kbmpti.jpg", link: "http://kbmpti.filkom.ub.ac.id/"},
+            { name: "SMK Negeri 4 Makassar", class: "skapat", img: "img/projects/smk4mks.jpg", link: "https://smkn4makassar.sch.id/"},
+            { name: "Alauddin Biomedical Olympiad", class: "abouinam", img: "img/projects/abouinam.jpg", link: "https://abouinam.com/"},
+            { name: "HMJ KIMIA UINAM", class: "hmjkimia", img: "img/projects/hmj-kimia.jpg", link: "https://hmjkimiauinalauddinmakassar.com/"},
+        ]
+    }
+})
